0b2154bbae82284642a9f008e6f2d6b8869ba1fa
Lynch <sarah.lynch@yale.edu>
2020-07-25 16:27:05 -0400

    removing global state

--- gatsby-theme-a11y-blog/gatsby-browser.js
+++ gatsby-theme-a11y-blog/gatsby-browser.js
@@ -1,13 +1,13 @@
-import "./src/styles/global.css";
-import React from "react";
-import GlobalContextProvider from "./src/context/GlobalContextProvider";
-import "focus-visible/dist/focus-visible";
-import PropTypes from 'prop-types';
+// import "./src/styles/global.css";
+// import React from "react";
+// import GlobalContextProvider from "./src/context/GlobalContextProvider";
+// import "focus-visible/dist/focus-visible";
+// import PropTypes from 'prop-types';
 
-export const wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>;
-};
+// export const wrapRootElement = ({ element }) => {
+//   return <GlobalContextProvider>{element}</GlobalContextProvider>;
+// };
 
-wrapRootElement.propTypes = {
-  element: PropTypes.object.isRequired,
-};
+// wrapRootElement.propTypes = {
+//   element: PropTypes.object.isRequired,
+// };
--- gatsby-theme-a11y-blog/src/pages/search-page.js
+++ gatsby-theme-a11y-blog/src/pages/search-page.js
@@ -1,20 +1,21 @@
 import React, { useContext } from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import SearchResults from "../components/search-results";
-import { GlobalStateContext } from "../context/GlobalContextProvider";
+// import SearchResults from "../components/search-results";
+// import { GlobalStateContext } from "../context/GlobalContextProvider";
 
 export default (SearchPage) => {
-  const state = useContext(GlobalStateContext);
-  const term = state.searchTerm.length
-    ? 'Search Results for "' + state.searchTerm + '"'
-    : "Enter a term you wish to search for";
-
+  // const state = useContext(GlobalStateContext);
+  // const term = state.searchTerm.length
+  //   ? 'Search Results for "' + state.searchTerm + '"'
+  //   : "Enter a term you wish to search for";
+const term = "hello"
   return (
     <Layout>
       <SEO title={term} />
-      <h1>{term}</h1>
-      <SearchResults />
+      hello
+      {/* <h1>{term}</h1>
+      <SearchResults /> */}
     </Layout>
   );
 };
