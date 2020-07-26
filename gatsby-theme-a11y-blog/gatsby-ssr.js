d89e9b84f9162e804ec59336b40b7845c5ee9a0b
Lynch <sarah.lynch@yale.edu>
2020-07-26 07:12:45 -0400

    ssr file

--- gatsby-theme-a11y-blog/gatsby-ssr.js
+++ gatsby-theme-a11y-blog/gatsby-ssr.js
@@ -2,13 +2,9 @@ import React from "react";
 import GlobalContextProvider from "./src/context/GlobalContextProvider";
 import PropTypes from 'prop-types';
 
-// export const wrapRootElement = ({ element }) => {
-//   return <GlobalContextProvider>{element}</GlobalContextProvider>;
-// };
-exports.wrapRootElement = ({ element }) => {
-    return <GlobalContextProvider>{element}</GlobalContextProvider>;
-  };
-
+export const wrapRootElement = ({ element }) => {
+  return <GlobalContextProvider>{element}</GlobalContextProvider>;
+};
 wrapRootElement.propTypes = {
   element: PropTypes.object.isRequired,
 };
