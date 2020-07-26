0f5c83dad863ef4b5d336fa35143eee033afb84c
Lynch <sarah.lynch@yale.edu>
2020-07-26 07:55:47 -0400

    adding 404

---
+++ gatsby-theme-a11y-blog/src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import image404 from "../images/404.jpeg";
+import { Styled } from "theme-ui";
+
+export default function PageNotFound(props) {
+  return (
+    <Layout>
+      <SEO title="Page Not Found" />
+      <Styled.h1>Page Not Found!</Styled.h1>
+       <img src={image404} alt="" /> 
+    </Layout>
+  );
+}
