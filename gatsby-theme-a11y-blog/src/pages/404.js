4ce1bfa7aeac8999356cbd4fd139ddf1312278c7
Lynch <sarah.lynch@yale.edu>
2020-07-26 10:07:46 -0400

    styling link on 404 page

--- gatsby-theme-a11y-blog/src/pages/404.js
+++ gatsby-theme-a11y-blog/src/pages/404.js
@@ -8,57 +8,68 @@ import Img from "gatsby-image";
 import { Styled } from "theme-ui";
 
 export function PageNotFound(props) {
-    
-    const text = props.data.allMarkdownRemark.nodes[0].frontmatter.text;
-    const featuredImage = props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImage;
-    const featuredImageAlt = props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageAlt;
+  const text = props.data.allMarkdownRemark.nodes[0].frontmatter.text;
+  const featuredImage =
+    props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImage;
+  const featuredImageAlt =
+    props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageAlt;
 
   return (
     <Layout>
       <SEO title="Page Not Found" />
       <Styled.h1>Page Not Found</Styled.h1>
       <p>{text}</p>
-      <p><Link to="/">Go Back Home</Link></p>
-      {featuredImage ? <Img fluid={featuredImage.childImageSharp.fluid} alt={featuredImageAlt} /> : null }
+      <p>
+        <Styled.a as={Link} to="/">
+          Go Back Home
+        </Styled.a>
+      </p>
+      {featuredImage ? (
+        <Img
+          fluid={featuredImage.childImageSharp.fluid}
+          alt={featuredImageAlt}
+        />
+      ) : null}
     </Layout>
   );
 }
 
 export default (props) => (
-    <StaticQuery
-      query={graphql`
-        query {
-         
-            allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/src/markdown/"}, frontmatter: {title: {eq: "Page Not Found"}}}) {
-                nodes {
-                  frontmatter {
-                    title
-                    text
-                    featuredImageAlt
-                    featuredImage {
-                      childImageSharp {
-                        fluid(maxWidth: 800) {
-                            src
-                            srcSet
-                            srcSetWebp
-                            base64
-                            aspectRatio
-                          }
-                      }
-                    }
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: {
+            fileAbsolutePath: { regex: "/src/markdown/" }
+            frontmatter: { title: { eq: "Page Not Found" } }
+          }
+        ) {
+          nodes {
+            frontmatter {
+              title
+              text
+              featuredImageAlt
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    src
+                    srcSet
+                    srcSetWebp
+                    base64
+                    aspectRatio
                   }
                 }
               }
             }
-          
-        
-      `}
-      render={(data) => <PageNotFound data={data} {...props} />}
-    />
-  );
-  PageNotFound.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.isRequired
-        }).isRequired
-  };
-  
+          }
+        }
+      }
+    `}
+    render={(data) => <PageNotFound data={data} {...props} />}
+  />
+);
+PageNotFound.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.isRequired,
+  }).isRequired,
+};
--- gatsby-theme-a11y-blog/src/pages/home-page.js
+++ gatsby-theme-a11y-blog/src/pages/home-page.js
@@ -9,12 +9,15 @@ import { Styled, Grid, jsx, Card } from "theme-ui";
 
 export function HomePage(props) {
   const blogListings = props.pageContext.blogs;
-   
-  const featuredImage = props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImage;
-  const featuredImageAlt = props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageAlt;
-  const featuredImageCaption = props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageCaption;
-  const html = props.data.allMarkdownRemark.nodes[0].html
-  console.log("props: ", props)
+
+  const featuredImage =
+    props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImage;
+  const featuredImageAlt =
+    props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageAlt;
+  const featuredImageCaption =
+    props.data.allMarkdownRemark.nodes[0].frontmatter.featuredImageCaption;
+  const html = props.data.allMarkdownRemark.nodes[0].html;
+
   return (
     <Layout>
       <SEO title="Home" />
@@ -22,80 +25,89 @@ export function HomePage(props) {
         <div>
           <Styled.h1>Welcome</Styled.h1>{" "}
           <Card variant="secondary">
-            <figure>{featuredImage ? <Img fluid={featuredImage.childImageSharp.fluid} alt={featuredImageAlt} /> : null }
-            
-            <figcaption>{featuredImageCaption}</figcaption></figure>
+            <figure>
+              {featuredImage ? (
+                <Img
+                  fluid={featuredImage.childImageSharp.fluid}
+                  alt={featuredImageAlt}
+                />
+              ) : null}
+
+              <figcaption>{featuredImageCaption}</figcaption>
+            </figure>
           </Card>
         </div>
         <div>
           <Styled.h2>About Me</Styled.h2>
 
           <p>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+            <div dangerouslySetInnerHTML={{ __html: html }} />
           </p>
         </div>
       </Grid>
       <div>
         <Styled.h2 sx={{ marginTop: "50px" }}>Most Recent Blog Posts</Styled.h2>
 
-        { blogListings ? blogListings.map((blog, index) => {
-          if (index <= 1) {
-            return (
-              <div key={index}>
-                <Styled.h3 sx={{ marginBottom: "0.5rem" }}>
-                  <Styled.a as={Link} to={blog.node.fields.slug}>
-                    {blog.node.frontmatter.title}
-                  </Styled.a>
-                </Styled.h3>
-                <p>{blog.node.excerpt}</p>
-              </div>
-            );
-          } else {
-            return null;
-          }
-        }) : null}
+        {blogListings
+          ? blogListings.map((blog, index) => {
+              if (index <= 1) {
+                return (
+                  <div key={index}>
+                    <Styled.h3 sx={{ marginBottom: "0.5rem" }}>
+                      <Styled.a as={Link} to={blog.node.fields.slug}>
+                        {blog.node.frontmatter.title}
+                      </Styled.a>
+                    </Styled.h3>
+                    <p>{blog.node.excerpt}</p>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })
+          : null}
       </div>
     </Layout>
   );
 }
 
-
 export default (props) => (
   <StaticQuery
     query={graphql`
       query {
-       
-          allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/src/markdown/"}, frontmatter: {title: {eq: "Home"}}}) {
-              nodes {
-                html
-                frontmatter {
-                  title
-                  featuredImageAlt
-                  featuredImageCaption
-                  featuredImage {
-                    childImageSharp {
-                      fluid(maxWidth: 800) {
-                          src
-                          srcSet
-                          srcSetWebp
-                          base64
-                          aspectRatio
-                        }
-                    }
+        allMarkdownRemark(
+          filter: {
+            fileAbsolutePath: { regex: "/src/markdown/" }
+            frontmatter: { title: { eq: "Home" } }
+          }
+        ) {
+          nodes {
+            html
+            frontmatter {
+              title
+              featuredImageAlt
+              featuredImageCaption
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    src
+                    srcSet
+                    srcSetWebp
+                    base64
+                    aspectRatio
                   }
                 }
               }
             }
           }
-        
-      
+        }
+      }
     `}
     render={(data) => <HomePage data={data} {...props} />}
   />
 );
 HomePage.propTypes = {
   data: PropTypes.shape({
-      allMarkdownRemark: PropTypes.isRequired
-      }).isRequired
+    allMarkdownRemark: PropTypes.isRequired,
+  }).isRequired,
 };
-
